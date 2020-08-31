bd3347aa5cf3c670abddcc2e9e3dd72e18e10c87
RitvikDayal-github <ritvikr1605@gmail.com>
2020-09-01 01:53:07 +0530

    added Feedbacks and Bugs in UI

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
-from .views import ProjectDetailView
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    path('project/<int:pk>/', views.project_detail, name='project-detail'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import *
+from bug_report.models import Bug
+from feedback.models import Feedback
 
 def home(request):
 
@@ -12,5 +14,16 @@ def home(request):
 
     return render(request, 'projects/index.html', context=context)
 
-class ProjectDetailView(DetailView):
-    model = Project
+def project_detail(request, pk):
+    projects = Project.objects.all()
+    bugs = Bug.objects.all()
+    feedbacks = Feedback.objects.all()
+
+    context ={
+        'projects': projects,
+        'bugs': bugs,
+        'feedbacks': feedbacks,
+        'project_id': pk
+    }
+
+    return render(request, 'projects/project_detail.html', context=context)
